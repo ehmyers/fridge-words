98020ac7af941a551b457422a033a52b95d5a5c2
Emily Myers <emilyhmyers@gmail.com>
2012-11-13 17:44:32 -0500

    added flowchart explanation, working on JS

--- fridge_words/externals/fridge_words.js
+++ fridge_words/externals/fridge_words.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+	// drag and drop code woop
 	function allowDrop(ev) {
 		ev.preventDefault();drag1
 	}
@@ -11,6 +13,13 @@ $(document).ready(function() {
 		ev.preventDefault();
 		var data=ev.dataTransfer.getData("Text");
 		ev.target.appendChild(document.getElementById(data));
+	}
+
+	var currentWord = ("hello");
+	// setting the width of the div based on text within
+	function size() {
+		var divLength = currentWord.length;
+		console.log(divLength);
 	}
 
 }
