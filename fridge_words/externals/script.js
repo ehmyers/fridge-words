81448986e93d056c246a29e09493d440307c7d9a
Emily Myers <emilyhmyers@gmail.com>
2012-12-09 22:19:08 -0500

    got the d/n swap working

--- fridge_words/externals/script.js
+++ fridge_words/externals/script.js
@@ -4,26 +4,35 @@ $(document).ready(function() {
 	var currentIndex = 0;
 	window.relativeX = 0;
 	window.relativeY = 0;
+	var fridge = true;
 
-	var words = {};
+	var words = [];
 	$.getJSON("externals/words.json", function(data) {
-		words = data;
-		console.log("The length of data is " + data.length);
+		words = data.words;
+		//console.log("The length of data is " + data.words.length);
 
 		// oulipo's n+7 constraint, still working on
+		// setInterval(function() {
+		// 	var randMag = $(".magnet")[Math.floor(Math.random()*numDivs)];
+		// 	var word = $(randMag).children().html();
+		// 	var wordpos = words[word].pos;
+		// 	console.log(wordpos);
+		// }, Math.random()*13000 + 2000);
+
+		// fri[d/n]ge words swap
 		setInterval(function() {
-			var randMag = $(".magnet")[Math.floor(Math.random()*numDivs)];
-			var word = $(randMag).children().html();
-			var wordpos = words[word].pos;
-			console.log(wordpos);
-		}, Math.random()*13000 + 2000);
+			$('#header').html('fringe words');
+		}, 5350); // <- 5.35 seconds
+		setInterval(function() {
+			$('#header').html('fridge words');
+		}, 5500); // <- 5.5 seconds
 
 		// creating the individual magnets
 		for (var i = 0; i < numDivs; i++) {
 			var randIndex = Math.floor(Math.random()*words.length);
 			var word = words[randIndex].word;
 			var newMagnet = $("<div class='drag magnet' data-index='" + randIndex + "'><span>" + word + "</span></div>");
-			
+
 			// randomizing the placement of the magnets
 			var browserWidth = $(window).width();
 			var browserHeight = $(window).height();
@@ -40,8 +49,8 @@ $(document).ready(function() {
 		// adding class on mousedown
 		$(".magnet").mousedown(function(e) {
 			var offset = $(this).offset();
- 		  window.relativeX = (e.pageX - offset.left);
- 		  window.relativeY = (e.pageY - offset.top);
+			window.relativeX = (e.pageX - offset.left);
+			window.relativeY = (e.pageY - offset.top);
 			$(this).addClass("dragging");
 			//console.log("dragging engaged!");
 
